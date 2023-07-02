4e2b41b30f80e2d2553a555543da1d0011d26f22
Nacho <nachomgoni@gmail.com>
2023-07-02 20:28:24 -0300

    new_proyect

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once "main.php"?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="Chart.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <div>
+            <h3>Seleccinar switches</h3>
+            <select name="switch1">
+                <option value="0">switch</option>
+                <?php host($conn);?>
+            </select>
+            <select name="switch2" style="margin-top: 5px">
+                <option value="0">switch</option>
+                <?php host($conn);?>
+            </select>
+            <button style="margin-top: 5px;">Cargar</button>
+        </div>
+    </form>
+    <button style="margin-top: 5px" onclick="grafico()">Ver grafico</button>
+    <div style="background-color: white; margin-top: 10px; width: 50%; height: 100%">
+        <canvas id="chart" hidden></canvas>
+    </div>
+</body>
+<script>
+    function grafico(){
+        document.getElementById("chart").innerHTML = 
+        `<canvas id="chart" style="width:250px"></canvas>`;
+        new Chart("chart", {
+            type: "line",
+            data: {
+                labels: [<?php for($i=0;$i<count($arrayX);$i++){echo $arrayX[$i].',';} ?>],
+                datasets: [{
+                    data: [<?php for($i=0;$i<count($arrayY1);$i++){echo $arrayY1[$i].',';} ?>],
+                    borderColor: "green",
+                    fill: false
+                },{
+                    data: [<?php for($i=0;$i<count($arrayY2);$i++){echo $arrayY2[$i].',';} ?>],
+                    borderColor: "red",
+                    fill: false
+                }]
+            },
+            options: {
+                legend: {display: false}
+            }
+        });
+    }
+</script>
+</html>
---
+++ main.php
@@ -0,0 +1,49 @@
+<?php
+
+function conexionBase(){
+    // Conexión con db = mysql_connect('ip_de_bd', 'usuario', 'clave', 'nombre_db')
+    $base = mysqli_connect('localhost', 'iperf', 'iperf', 'iperf'); // Conexion con la base
+    return $base;
+}
+
+$conn = conexionBase();
+
+function host($conexion){
+    $resp = mysqli_query($conexion,"select switch from switches");
+    while ($lista = mysqli_fetch_array($resp)){
+        echo '<option value = "'.$lista['switch'].'">'.$lista['switch'].'</option>';
+    };
+};
+
+// Crear arrays con los valores x y el value y
+function valoresX($enlace){
+    $values = array();
+    $resp = mysqli_query($enlace, "select distinct dia from diario_ps");
+    while($column = mysqli_fetch_array($resp)){
+        $ver = $column['dia'];
+        $values[] = json_encode($ver);
+    }
+    return $values;
+}
+
+function valoresY($enlace,$switch){
+    $arrayY = array();
+    $resp = mysqli_query($enlace, 'select medido from diario_ps where ubicacion = "'.$switch.'"');
+    while($column = mysqli_fetch_array($resp)){
+        $arrayY[] = json_encode($column['medido']);
+    }
+    return $arrayY;
+}
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $switch1 = $_POST['switch1'];
+    $switch2 = $_POST['switch2'];
+    $arrayX = array();
+    $arrayY1 = array();
+    $arrayY2 = array();
+    $arrayX=valoresX($conn);
+    $arrayY1 = valoresY($conn,$switch1);
+    $arrayY2 = valoresY($conn,$switch2);
+}
+?>
